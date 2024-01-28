dfc73237de6f86144420511835ff381a0ee3ae1e
gdaglio <gabriele.daglio@mail.polimi.it>
2024-01-28 12:05:24 +0100

    first commit

---
+++ script.py
@@ -0,0 +1,23 @@
+import webbrowser
+import pyautogui as py
+import time
+import random
+import requests
+
+words_site = "https://www.mit.edu/~ecprice/wordlist.10000"
+
+response = requests.get(words_site)
+words = response.content.decode('UTF-8').splitlines()
+random.shuffle(words)
+words = words[:30]
+
+
+browser = webbrowser.get('windows-default')
+browser.open("www.bing.com", new=0)
+time.sleep(2)
+
+for word in words:
+    
+    py.write(word, interval=0.1)
+    py.keyDown("return")
+
